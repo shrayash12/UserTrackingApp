6d80dc67af54505c900fabb490cf3e49d35e46a0
shryash12 <dr.shradhasrathod@gmail.com>
2021-03-31 01:00:03 +0800

    ForgotPasswordActivity findViewById implemented

--- app/src/main/java/shradha/com/finalloginsignupauthproj/ForgotPasswordActivity.java
+++ app/src/main/java/shradha/com/finalloginsignupauthproj/ForgotPasswordActivity.java
@@ -2,13 +2,21 @@ package shradha.com.finalloginsignupauthproj;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class ForgotPasswordActivity extends AppCompatActivity {
+    EditText resetPassword_Email;
+    Button btn_ResetPassword;
 
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgot_password);
+        resetPassword_Email = findViewById(R.id.resetPassword_Email);
+        btn_ResetPassword = findViewById(R.id.btn_ResetPassword);
     }
 }
