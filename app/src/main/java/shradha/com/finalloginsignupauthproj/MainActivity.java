7e8b2fb8ad37626b5e0371f04438d1bb14f45412
shryash12 <dr.shradhasrathod@gmail.com>
2021-03-31 01:01:10 +0800

    commit

--- app/src/main/java/shradha/com/finalloginsignupauthproj/MainActivity.java
+++ app/src/main/java/shradha/com/finalloginsignupauthproj/MainActivity.java
@@ -6,11 +6,13 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
