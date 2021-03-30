e5e8b2a748c1c939cf3b492e7ec30ef6c000400a
shryash12 <dr.shradhasrathod@gmail.com>
2021-03-30 16:54:41 +0800

    maintaining Activities

--- app/src/main/java/shradha/com/finalloginsignupauthproj/MainActivity.java
+++ app/src/main/java/shradha/com/finalloginsignupauthproj/MainActivity.java
@@ -6,14 +6,11 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
--- app/src/main/java/shradha/com/finalloginsignupauthproj/SignUpActivity.java
+++ app/src/main/java/shradha/com/finalloginsignupauthproj/SignUpActivity.java
@@ -2,16 +2,12 @@ package shradha.com.finalloginsignupauthproj;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.Task;
