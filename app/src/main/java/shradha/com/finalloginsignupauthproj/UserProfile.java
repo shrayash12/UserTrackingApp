39b3ca50ec17b3e147563f69359ad327d0aa1157
shryash12 <dr.shradhasrathod@gmail.com>
2021-04-01 15:11:57 +0800

    retrieving data from firebase successful

--- app/src/main/java/shradha/com/finalloginsignupauthproj/UserProfile.java
+++ app/src/main/java/shradha/com/finalloginsignupauthproj/UserProfile.java
@@ -1,14 +1,60 @@
 package shradha.com.finalloginsignupauthproj;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class UserProfile extends AppCompatActivity {
+    FirebaseUser firebaseUser;
+    DatabaseReference reference;
+    String userId;
+    TextView tv_Name;
+    TextView tv_Age;
+    TextView tv_EmailId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
+        tv_Name = findViewById(R.id.tv_Name);
+        tv_Age = findViewById(R.id.tv_Age);
+        tv_EmailId = findViewById(R.id.tv_EmailId);
+
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        reference = FirebaseDatabase.getInstance().getReference("User");
+        userId = firebaseUser.getUid();
+        reference.child(userId).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                User user = snapshot.getValue(User.class);
+                if (user != null) {
+                    String name = user.userName;
+                    String email = user.email;
+                    String age = user.age;
+
+                    tv_Name.setText(name);
+                    tv_Age.setText(age + " yrs");
+                    tv_EmailId.setText(email);
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+                Toast.makeText(UserProfile.this, "Something Happened wrong", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
