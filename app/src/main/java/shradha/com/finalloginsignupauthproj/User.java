39608406a379ed7f6bb8ffe9f0b5ea182021dea1
shryash12 <dr.shradhasrathod@gmail.com>
2021-04-06 21:40:02 +0800

    user id added in user

--- app/src/main/java/shradha/com/finalloginsignupauthproj/User.java
+++ app/src/main/java/shradha/com/finalloginsignupauthproj/User.java
@@ -4,14 +4,17 @@ public class User {
     public String userName;
     public String age;
     public String email;
+    public int uId;
 
     public User() {
 
     }
 
-    public User(String userName, String age, String email) {
+    public User(String userName, String age, String email, int uId) {
         this.userName = userName;
         this.age = age;
         this.email = email;
+        this.uId = uId;
     }
+
 }
