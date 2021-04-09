226cf097078e9348d87d49742aa5d919295b97b6
shryash12 <dr.shradhasrathod@gmail.com>
2021-04-09 21:51:34 +0800

    getting data from cloud fireStore in UserProfile

--- app/src/main/java/shradha/com/finalloginsignupauthproj/UserProfile.java
+++ app/src/main/java/shradha/com/finalloginsignupauthproj/UserProfile.java
@@ -7,6 +7,8 @@ import android.os.Bundle;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -14,14 +16,20 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class UserProfile extends AppCompatActivity {
-    FirebaseUser firebaseUser;
-    DatabaseReference reference;
+    FirebaseAuth mAuth;
+    FirebaseFirestore firestore;
+    DocumentReference documentReference;
+
     String userId;
     TextView tv_Name;
     TextView tv_Age;
     TextView tv_EmailId;
+    TextView tvTitle_User;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,30 +38,21 @@ public class UserProfile extends AppCompatActivity {
         tv_Name = findViewById(R.id.tv_Name);
         tv_Age = findViewById(R.id.tv_Age);
         tv_EmailId = findViewById(R.id.tv_EmailId);
+        tvTitle_User = findViewById(R.id.tvTitle_User);
 
-        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
-        reference = FirebaseDatabase.getInstance().getReference("User");
-        userId = firebaseUser.getUid();
-        reference.child(userId).addListenerForSingleValueEvent(new ValueEventListener() {
+        FirebaseUser firebaseUser = mAuth.getCurrentUser();
+        documentReference = firestore.collection("User").document(firebaseUser.getUid());
+
+        documentReference.get().addOnSuccessListener(UserProfile.this, new OnSuccessListener<DocumentSnapshot>() {
             @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                User user = snapshot.getValue(User.class);
-                if (user != null) {
-                    String name = user.userName;
-                    String email = user.email;
-                    String age = user.age;
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
 
-                    tv_Name.setText(name);
-                    tv_Age.setText(age + " yrs");
-                    tv_EmailId.setText(email);
 
-                }
             }
-
+        }).addOnFailureListener(UserProfile.this, new OnFailureListener() {
             @Override
-            public void onCancelled(@NonNull DatabaseError error) {
+            public void onFailure(@NonNull Exception e) {
 
-                Toast.makeText(UserProfile.this, "Something Happened wrong", Toast.LENGTH_SHORT).show();
             }
         });
     }
