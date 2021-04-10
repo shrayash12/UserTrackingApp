b4fb6d6fff95605bd52c7c15247059e7bd860ca2
shryash12 <dr.shradhasrathod@gmail.com>
2021-04-10 17:12:43 +0800

    Constants class created for keyWords

---
+++ app/src/main/java/shradha/com/finalloginsignupauthproj/Constants.java
@@ -0,0 +1,8 @@
+package shradha.com.finalloginsignupauthproj;
+
+public class Constants {
+    public static final String KEY_AGE = "UseAge";
+    public static final String KEY_NAME = "UseName";
+    public static final String KEY_EMAIL = "UserEmail";
+
+}
